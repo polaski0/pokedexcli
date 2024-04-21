962f9aa3b17c5f7a53454cf829413570f9da709e
Meynard Trinidad <meynard.trinidad@diversifiedinfra.com>
2024-04-21 15:56:57 +0800

    Added cache

--- internal/api.go
+++ internal/api.go
@@ -4,44 +4,58 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
 )
 
 const API_BASE_PATH = "https://pokeapi.co/api/v2"
 const LOCATION_AREA_PATH = API_BASE_PATH + "/location-area"
 
+type Result struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
 type LocationArea struct {
-	Count    int     `json:"count"`
-	Next     *string `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
+	Count    int      `json:"count"`
+	Next     *string  `json:"next"`
+	Previous *string  `json:"previous"`
+	Results  []Result `json:"results"`
 }
 
 func GetLocationArea(pageUrl *string) (LocationArea, error) {
+	var locationArea LocationArea
 	url := LOCATION_AREA_PATH
+	cache := NewCache(5 * time.Second)
+
 	if pageUrl != nil {
 		url = *pageUrl
 	}
 
-	res, err := http.Get(url)
-	if err != nil {
-		return LocationArea{}, err
+	if res, ok := cache.Get(url); ok {
+		err := json.Unmarshal(res, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		return locationArea, nil
+	} else {
+		res, err := http.Get(url)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		body, err := io.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		err = json.Unmarshal(body, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+        cache.Add(url, body)
+		return locationArea, nil
 	}
-
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		return LocationArea{}, err
-	}
-
-	var locationArea LocationArea
-	err = json.Unmarshal(body, &locationArea)
-
-	if err != nil {
-		return LocationArea{}, err
-	}
-
-	return locationArea, err
 }
---
+++ internal/cache.go
@@ -0,0 +1,49 @@
+package internal
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	Entry map[string]CacheEntry
+	mu    *sync.Mutex
+}
+
+type CacheEntry struct {
+	CreatedAt time.Time
+	Val       []byte
+}
+
+func NewCache(d time.Duration) Cache {
+	cache := Cache{
+		Entry: make(map[string]CacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	// ticker := time.NewTicker(d)
+	// defer ticker.Stop()
+	reapLoop()
+
+	return cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.Entry[key] = CacheEntry{
+		Val:       val,
+		CreatedAt: time.Now(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	if m, ok := c.Entry[key]; ok {
+		return m.Val, true
+	}
+
+	return []byte{}, false
+}
+
+// Is called once a new cache is created.
+// Keeps checking based on the interval and remove any entries
+// older than the interval.
+func reapLoop() {}
